92b9e4fdc9db820ed48ef70017f03c7f8cc566da
Eitan Suez <eitan.suez@gmail.com>
2017-01-30 15:59:55 -0600

    recast two integration tests after upping spring boot version also upped version of spring mongo driver

--- src/test/java/org/springframework/cloud/servicebroker/mongodb/service/MongoAdminServiceIntegrationTest.java
+++ src/test/java/org/springframework/cloud/servicebroker/mongodb/service/MongoAdminServiceIntegrationTest.java
@@ -1,21 +1,16 @@
 package org.springframework.cloud.servicebroker.mongodb.service;
 
-import com.mongodb.BasicDBList;
-import com.mongodb.BasicDBObjectBuilder;
-import com.mongodb.CommandResult;
-import com.mongodb.DB;
-import com.mongodb.DBObject;
-import com.mongodb.MongoClient;
+import com.mongodb.*;
 import org.junit.After;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.servicebroker.mongodb.IntegrationTestBase;
 import org.springframework.cloud.servicebroker.mongodb.exception.MongoServiceException;
-import org.springframework.test.annotation.DirtiesContext;
 
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertTrue;
+import java.util.List;
+
+import static java.util.Collections.singletonList;
+import static org.junit.Assert.*;
 
 
 public class MongoAdminServiceIntegrationTest extends IntegrationTestBase {
@@ -31,7 +26,7 @@ public class MongoAdminServiceIntegrationTest extends IntegrationTestBase {
 		client.getDB(DB_NAME).command("dropAllUsersFromDatabase");
 		client.dropDatabase(DB_NAME);
 	}
-	
+
 	@Test
 	public void instanceCreationIsSuccessful() throws MongoServiceException {
 		DB db = service.createDatabase(DB_NAME);
@@ -59,15 +54,19 @@ public class MongoAdminServiceIntegrationTest extends IntegrationTestBase {
 	}
 	
 	@Test
-	@DirtiesContext // because we can't authenticate twice on same DB
 	public void newUserCreatedSuccessfully() throws MongoServiceException {
 		service.createDatabase(DB_NAME);
 		service.createUser(DB_NAME, "user", "password");
-		assertTrue(client.getDB(DB_NAME).authenticate("user", "password".toCharArray()));
+
+		// by querying db with new credentials, can vet that credentials work
+		new MongoClient(
+				singletonList(new ServerAddress("localhost", 27017)),
+				singletonList(MongoCredential.createCredential("user", DB_NAME, "password".toCharArray()))
+		).getDatabase(DB_NAME).listCollectionNames().first();
+
 	}
 	
-	@Test
-	@DirtiesContext // because we can't authenticate twice on same DB
+	@Test(expected = MongoTimeoutException.class )
 	public void deleteUserSucceeds() throws MongoServiceException {
 		service.createDatabase(DB_NAME);
 		DBObject createUserCmd = BasicDBObjectBuilder.start("createUser", "user").add("pwd", "password")
@@ -75,7 +74,12 @@ public class MongoAdminServiceIntegrationTest extends IntegrationTestBase {
 		CommandResult result = client.getDB(DB_NAME).command(createUserCmd);
 		assertTrue("create should succeed", result.ok());
 		service.deleteUser(DB_NAME, "user");
-		assertFalse(client.getDB(DB_NAME).authenticate("user", "password".toCharArray()));
+
+		new MongoClient(
+				singletonList(new ServerAddress("localhost", 27017)),
+				singletonList(MongoCredential.createCredential("user", DB_NAME, "password".toCharArray())),
+				new MongoClientOptions.Builder().serverSelectionTimeout(500).build()
+		).getDatabase(DB_NAME).listCollectionNames().first();
 	}
 	
 }
